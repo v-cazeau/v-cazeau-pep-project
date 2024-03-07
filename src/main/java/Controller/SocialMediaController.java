3cc3e22f6219dc86255bd441f7137e50d6576463
Veronie Cazeau <veronie.cazeau@gmail.com>
2024-03-07 22:23:23 +0000

    Updated Controller paths & imported Models and Services

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package Controller;
 
+import static org.mockito.Mockito.lenient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Model.Message;
+import Service.AccountService;
+import Service.MessageService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -7,6 +16,15 @@ import io.javalin.http.Context;
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ * 
+ *  GET localhost:8080/accounts/{account_id}/messages : list all posts from a particular user
+ *  POST localhost:8080/register : create a new account
+ *  POST localhost:8080/login : verify login
+ *  GET localhost:8080/messages : retrieve all messages
+ *  GET localhost:8080/messages/{message_id} : retrieve all messages by Id
+ *  POST localhost:8080/messages : create a new message
+ *  PATCH localhost:8080/messages/{message_id} : update a message
+ *  DELETE localhost:8080/messages/{message_id} : delete message
  */
 public class SocialMediaController {
     /**
@@ -16,8 +34,14 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
-
+        app.get("/accounts/{account_id}", this::getAllMessagesFromUserHandler);
+        app.post("/register", this::postAccountHandler);
+        app.post("login", this::postLoginHandler);
+        app.get("/messages", this::getAllMessagesHandler);
+        app.get("/messages/{message_id}", this::getAllMessagesByIdHandler);
+        app.post("/messages", this::postMessageHandler); 
+        app.patch("/messages/{message_id}", this::patchMessageHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageHandler);
         return app;
     }
 
@@ -25,9 +49,12 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
-    }
+  
+
+
+    // private void exampleHandler(Context context) {
+    //     context.json("sample text");
+    // }
 
 
 }
